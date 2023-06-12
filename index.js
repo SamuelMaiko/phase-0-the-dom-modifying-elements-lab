f14f69b51eaa7dfaff42b0f16a4fd8813ebed0ce
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-12 17:10:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const epic=document.querySelector("main");
+epic.remove();
+
+//const newHeader=document.createElement("h1");
+//document.body.append(newHeader);
+//newHeader.classList.add(" victory");
+
+const newHeader=document.querySelector("h1");
+
+newHeader.textContent = "Sammy Maiko is the champion";
